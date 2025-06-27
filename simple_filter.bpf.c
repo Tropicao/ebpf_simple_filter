0bead97d12ecf6863235ab850f28e770ddd9eaba
Alexis Lothoré <alexis.lothore@bootlin.com>
2025-06-27 10:26:30 +0200

    bpf: also reject ICMP6 over IPv6
    The current program is only monitoring ICMP packets over IPv4. However,
    the ping command may be using ipv6 by default when emitting traffic:
    
    PING localhost (::1) from ::1 lo: 56 data bytes
    
    In this case, the pings are still succeeding despite the filter being
    enabled.
    
    Handle the ipv6/icmpv6 case as well.
    
    Signed-off-by: Alexis Lothoré <alexis.lothore@bootlin.com>

--- simple_filter.bpf.c
+++ simple_filter.bpf.c
@@ -1,7 +1,9 @@
 #include <linux/bpf.h>
 #include <linux/if_ether.h>
+#include <linux/in6.h>
 #include <linux/ip.h>
 #include <linux/in.h>
+#include <linux/ipv6.h>
 #include <bpf/bpf_endian.h>
 #include <bpf/bpf_helpers.h>
 
@@ -18,23 +20,33 @@ int drop_icmp(struct xdp_md *xdp)
 	void *data_end = (void *)(long)xdp->data_end;
 	void *data = (void *)(long)xdp->data;
 	struct ethhdr *eth = data;
+	struct ipv6hdr *ip6;
 	struct iphdr *ip;
 	int *count;
 	int key=0;
+	__u8 protocol;
 
 	if (eth + 1 > data_end)
 		return XDP_DROP;
 
-	if (eth->h_proto != bpf_htons(ETH_P_IP))
-		return XDP_PASS;
-
-	ip = data+sizeof(struct ethhdr);
-
-	if (ip + 1 > data_end)
-		return XDP_DROP;
-
-	if (ip->protocol != IPPROTO_ICMP)
-		return XDP_PASS;
+	switch (eth->h_proto) {
+		case bpf_htons(ETH_P_IP):
+			ip = data+sizeof(struct ethhdr);
+			if (ip + 1 > data_end)
+				return XDP_DROP;
+			if (ip->protocol != IPPROTO_ICMP)
+				return XDP_PASS;
+			break;
+		case bpf_htons(ETH_P_IPV6):
+			ip6 = data+sizeof(struct ethhdr);
+			if (ip6 + 1 > data_end)
+				return XDP_DROP;
+			if (ip6->nexthdr != IPPROTO_ICMPV6)
+				return XDP_PASS;
+			break;
+		default:
+			return XDP_PASS;
+	}
 
 	char fmt[] = "Dropping ICMP packet !";
 	bpf_trace_printk(fmt, sizeof(fmt));
